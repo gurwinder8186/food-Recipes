82abfa31b33c24277e355b907dd382362b0c81fa
Nikki <178754087+nikki-cupples@users.noreply.github.com>
2024-11-08 13:50:13 +1300

    add error and loading components

---
+++ client/components/ErrorMessage.tsx
@@ -0,0 +1,10 @@
+export default function Error() {
+  return (
+    <>
+      <section className="">
+        <p></p>
+        <img src="" alt="" />
+      </section>
+    </>
+  )
+}
---
+++ client/components/Loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  return (
+    <>
+      <section className="shake-it">
+        <p></p>
+        <img src="images/saltandpepper.gif" alt="salt and pepper shaking" />
+      </section>
+    </>
+  )
+}
