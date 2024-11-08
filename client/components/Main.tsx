445676d082f80b144a190d1e33a75bb2c2ca1b29
Nikki <178754087+nikki-cupples@users.noreply.github.com>
2024-11-08 13:22:24 +1300

    add nav bar to home

--- client/components/Main.tsx
+++ client/components/Main.tsx
@@ -2,10 +2,12 @@
 // HERE WILL BE THE FIRST HOME PAGE
 import { Outlet } from 'react-router-dom'
 import RecipeSearch from './SearchBar'
+import Nav from './Nav'
 
 export default function Main() {
   return (
     <>
+      <Nav />
       <RecipeSearch />
       <Outlet />
     </>
